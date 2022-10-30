bce3ec00e36c69d10c036463415b9483cf320a5b
Daniel Torres <denial.torres@gmail.com>
2022-10-30 11:30:37 -0600

    bound the queues and the exchanges creates

--- connector.rb
+++ connector.rb
@@ -16,11 +16,34 @@ require 'dotenv'
 Dotenv.load
 
 begin
+  # 2. Read RABBIT_URI from ENV
   connection = Bunny.new ENV["RABBIT_URI"]
+
+  # 3, Start a communication session with RabbitMQ
   connection.start
+  # 3.1 Declare a channel
+  channel = connection.create_channel
 
   # allow reconnection recovery
   sleep 10
 rescue Bunny::TCPConectionFailed => e
   puts "conection to server failed #{e}"
 end
+
+def on_start(channel)
+  # 4. Declare a queue for a given taxi
+  queue = channel.queue("taxi.1", durable: true)
+
+  # 5. Declare a "direct exchange" => "taxi-direct"
+  exchange = channel.direct("taxi-direct", durable: true, auto_delete: true)
+
+  # 6. bind/join the queue to the exchange
+  queue.bind(exchange, routing_key: "taxi.1")
+
+  # 7. Return the exchange
+  exchange
+end
+
+binding.pry
+
+exchange = on_start(channel) 
